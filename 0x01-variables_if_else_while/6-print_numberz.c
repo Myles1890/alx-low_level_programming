c76cb225b05ba01cd0ff74640b8c36cc07b9ded8
myles1890 <nbcttc@yahoo.com>
2023-09-09 10:52:46 -0700

    number printing

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing of numners
+ *
+ * Return: can be 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
+}
+
