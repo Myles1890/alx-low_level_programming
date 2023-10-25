9b92c7a38ac261c8ad5b93d367a36e01ad607b16
myles1890 <nbcttc@yahoo.com>
2023-10-24 17:42:44 -0700

    more singly linked list task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
  size_t number = 0;
 
- while (d)
+ while (h)
  {
- printf("%d\n", d->n);
+ printf("%d\n", h->n);
  number++;
- d = d->next;
+ h = h->next;
  }
 
  return (number);
